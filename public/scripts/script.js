1256ea5a3b819636260d534021cce040c1e26d52
brainy <devbrainy98@gmail.com>
2022-03-30 07:07:11 +0000

    updated various files

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,16 +1,19 @@
 // declare variables
 let toogleBtn = document.querySelector('#toogle-btn');
 let navMenu = document.querySelector('#nav-menu');
-
+let socialMenu = document.querySelector('#social-menu');
 
 // add event listeners
-toogleBtn.addEventListener('click', showNav);
+toogleBtn.addEventListener('click', showNav); 
 
 // add functions
 function showNav() {
     if(navMenu.classList.contains('hidden')){
         navMenu.classList.remove('hidden');
+        socialMenu.classList.remove('hidden');
     }else{
         navMenu.classList.add('hidden');
+        socialMenu.classList.add('hidden');
+
     }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'black-shade': 'linear-gradient(180deg, #302E2E 0%, #242323 9%, #292424 68%, #312E2E 100%)',
+      },
+      colors: {
+        'primary': '#4D106B',
+        'secondary': '#7C15AC',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+      container: {
+        center: true,
+        padding: '5rem',
+      },
+    },
+  },
+  plugins: [],
+}
+

5f5898c4aaea3831b2ca1263d37d06ce77cd322b
brainy <devbrainy98@gmail.com>
2022-03-31 07:01:55 +0000

    updated everything

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -23,32 +23,38 @@ function showNav() {
 }
 
 // Youtube lite video on-demand
-function labnolIframe(div) {
-    var iframe = document.createElement('iframe');
-    iframe.setAttribute('src', 'https://www.youtube.com/embed/' + div.dataset.id + '?autoplay=1&rel=0');
-    iframe.setAttribute('frameborder', '0');
-    iframe.setAttribute('allowfullscreen', '1');
-    iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
-    div.parentNode.replaceChild(iframe, div);
-  }
 
-  function initYouTubeVideos() {
-    var playerElements = document.getElementsByClassName('youtube-player');
-    for (var n = 0; n < playerElements.length; n++) {
-      var videoId = playerElements[n].dataset.id;
-      var div = document.createElement('div');
-      div.setAttribute('data-id', videoId);
-      var thumbNode = document.createElement('img');
-      thumbNode.src = '//i.ytimg.com/vi/ID/hqdefault.jpg'.replace('ID', videoId);
-      div.appendChild(thumbNode);
-      var playButton = document.createElement('div');
-      playButton.setAttribute('class', 'play');
-      div.appendChild(playButton);
-      div.onclick = function () {
-        labnolIframe(this);
-      };
-      playerElements[n].appendChild(div);
-    }
-  }
+ // Find all the YouTube video embedded on a page
+ let videos = document.getElementsByClassName('youtube');
+ videos = Array.from(videos);
+ let youtube = "";
 
-  document.addEventListener('DOMContentLoaded', initYouTubeVideos);
+   videos.forEach(video =>{
+       youtube = video;
+
+       // Based on the YouTube ID, we can easily find the thumbnail image
+    let img = document.createElement('img');
+    img.setAttribute('src', `http://i.ytimg.com/vi/${youtube.id}/hqdefault.jpg`);
+    img.setAttribute('class', 'thumb');
+
+    // Overlay the Play icon to make it look like a video player
+    let play = document.createElement('div');
+    play.setAttribute('class', 'play');
+
+    youtube.appendChild(img);
+    youtube.appendChild(play);
+
+    // Attach an onclick event to the YouTube Thumbnail
+    youtube.onclick = function () {
+        // Create an iFrame with autoplay set to true
+        let iframe = document.createElement('iframe');
+        iframe.setAttribute('src', `https://www.youtube.com/embed/${this.id}?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1`);
+
+        // The height and width of the iFrame should be the same as parent
+        iframe.style.width = this.style.width;
+        iframe.style.height = this.style.height;
+
+        // Replace the YouTube thumbnail with YouTube HTML5 Player
+        this.parentNode.replaceChild(iframe, this);
+    };
+   });
